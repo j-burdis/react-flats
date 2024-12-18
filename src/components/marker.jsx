ca2bd29e53a5bf6e0cb9c6d352703910fa8b5466
Jonathan Burdis <jonnyburdis@hotmail.com>
2024-12-18 15:25:25 +0000

    remove css redundancy

--- src/components/marker.jsx
+++ src/components/marker.jsx
@@ -4,12 +4,6 @@ import React from 'react';
 const Marker = () => (
   <div
     className="marker"
-    style={{
-      height: '20px',
-      width: '20px',
-      backgroundColor: 'red',
-      borderRadius: '50%'
-    }}
   />
 );
 
